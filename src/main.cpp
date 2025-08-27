5448545c2241ab6a8dfd2470c7c76947943d3f02
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-08-27 06:47:59 -0300

    feat: Adicionando suporte a acentuação
    Está sendo configurado o locale e as mensagens de erro e informação

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include "exec/ExecManager.h"
 #include "io/io.h"
 
-#include <iostream>
-using namespace std;
+#include <stdexcept>
+#include <locale>
 
-int main( int argc, char* argv[] ) {    
+const string PT_BR_LOCALE = "pt_BR.UTF-8";
+const string DEFAULT_LOCALE = "";
+
+int main( int argc, char* argv[] ) {  
+    try {
+        std::locale testlocale( PT_BR_LOCALE );
+        setlocale( LC_ALL, PT_BR_LOCALE.c_str() );
+    } catch ( const std::runtime_error& ) {
+        setlocale( LC_ALL, DEFAULT_LOCALE.c_str() );
+    }
+    
     ExecManager* manager = new ExecManager();
     manager->exec( argc, argv );
 
