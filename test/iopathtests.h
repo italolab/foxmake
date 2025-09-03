2a2d75c9827b06451b86de73bde35955ca889dfe
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-09-03 15:39:54 -0300

    fix: Consertando Limitação de Bloco de Código das Macros de Assert
    Havia um problema/limitação nas macros de assert do CBTest. Onde,
    não se podia chamar a macro de assert, por exemplo, dentro de um if com
    else e sem abre e fecha chave como bloco entre o if e o else.
    
    O problema foi consertado no CBTest e a lib foi atualizada no CBuild

--- test/iopathtests.h
+++ test/iopathtests.h
@@ -13,181 +13,180 @@ using std::string;
 using namespace std;
 
 TEST_CASE( dirPathTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::dirPath( "C:/__xx" ), "C:", );
-    ASSERT_EQUALS( io::path::dirPath( "C:__xx" ), "C:", );
-    ASSERT_EQUALS( io::path::dirPath( "C:" ), "", );
-    ASSERT_EQUALS( io::path::dirPath( "a/b/c" ), "a/b", );
-    ASSERT_EQUALS( io::path::dirPath( "a/b/c/" ), "a/b", );
-    ASSERT_EQUALS( io::path::dirPath( "/a/b" ), "/a", );
-    ASSERT_EQUALS( io::path::dirPath( "/__xx" ), "/", );
-    ASSERT_EQUALS( io::path::dirPath( "/" ), "/", );
-    ASSERT_EQUALS( io::path::dirPath( "" ), "", );
+    ASSERT_EQUALS( io::path::dirPath( "C:/__xx" ), "C:", )
+    ASSERT_EQUALS( io::path::dirPath( "C:__xx" ), "C:", )
+    ASSERT_EQUALS( io::path::dirPath( "C:" ), "", )
+    ASSERT_EQUALS( io::path::dirPath( "a/b/c" ), "a/b", )
+    ASSERT_EQUALS( io::path::dirPath( "a/b/c/" ), "a/b", )
+    ASSERT_EQUALS( io::path::dirPath( "/a/b" ), "/a", )
+    ASSERT_EQUALS( io::path::dirPath( "/__xx" ), "/", )
+    ASSERT_EQUALS( io::path::dirPath( "/" ), "/", )
+    ASSERT_EQUALS( io::path::dirPath( "" ), "", )
 
-    if ( io::fileExists( "C:/" ) ) {
-        ASSERT_EQUALS( io::path::dirPath( "C:/" ), "C:", );
-    } else {
-        ASSERT_EQUALS( io::path::dirPath( "C:/" ), "", );    
-    }
+    if ( io::fileExists( "C:/" ) )
+        ASSERT_EQUALS( io::path::dirPath( "C:/" ), "C:", )
+    else
+        ASSERT_EQUALS( io::path::dirPath( "C:/" ), "", )       
 }
 
 TEST_CASE( recursiveDirPathToReplaceTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**/c/d" ), "a/b/",  );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "**/a/b/c" ), "",  );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "/**/a/b/c" ), "/",  );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "/a/**/b/c" ), "/a/",  );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "" ), "",  );
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**/c/d" ), "a/b/", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "**/a/b/c" ), "", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "/**/a/b/c" ), "/", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "/a/**/b/c" ), "/a/", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "" ), "", )
 }
 
 TEST_CASE( fileOrDirNameTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::fileOrDirName( "a" ), "a", );
-    ASSERT_EQUALS( io::path::fileOrDirName( "a/a/b" ), "b", );
-    ASSERT_EQUALS( io::path::fileOrDirName( "a/a/b.txt" ), "b.txt", );
-    ASSERT_EQUALS( io::path::fileOrDirName( "/*.txt" ), "*.txt", );
-    ASSERT_EQUALS( io::path::fileOrDirName( "/*a*.*" ), "*a*.*", );
-    ASSERT_EQUALS( io::path::fileOrDirName( "" ), "", );
+    ASSERT_EQUALS( io::path::fileOrDirName( "a" ), "a", )
+    ASSERT_EQUALS( io::path::fileOrDirName( "a/a/b" ), "b", )
+    ASSERT_EQUALS( io::path::fileOrDirName( "a/a/b.txt" ), "b.txt", )
+    ASSERT_EQUALS( io::path::fileOrDirName( "/*.txt" ), "*.txt", )
+    ASSERT_EQUALS( io::path::fileOrDirName( "/*a*.*" ), "*a*.*", )
+    ASSERT_EQUALS( io::path::fileOrDirName( "" ), "", )
 }
 
 TEST_CASE( fileOrDirNameWithoutExtensionTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "a" ), "a", );
-    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "a/a/b" ), "b", );
-    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "a/a/b.txt" ), "b", );
-    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "/*" ), "*", );
-    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "/*a*" ), "*a*", );
-    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "" ), "", );
+    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "a" ), "a", )
+    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "a/a/b" ), "b", )
+    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "a/a/b.txt" ), "b", )
+    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "/*" ), "*", )
+    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "/*a*" ), "*a*", )
+    ASSERT_EQUALS( io::path::fileOrDirNameWithoutExtension( "" ), "", )
 }
 
 TEST_CASE( makeUnixPreferredTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::makeUnixPreferred( "C:\\a\\b" ), "C:/a/b", );
-    ASSERT_EQUALS( io::path::makeUnixPreferred( "C:" ), "C:", );
-    ASSERT_EQUALS( io::path::makeUnixPreferred( "C:\\a/b/c\\d" ), "C:/a/b/c/d", );
-    ASSERT_EQUALS( io::path::makeUnixPreferred( "a\\b/c\\d" ), "a/b/c/d", );
-    ASSERT_EQUALS( io::path::makeUnixPreferred( "" ), "", );
+    ASSERT_EQUALS( io::path::makeUnixPreferred( "C:\\a\\b" ), "C:/a/b", )
+    ASSERT_EQUALS( io::path::makeUnixPreferred( "C:" ), "C:", )
+    ASSERT_EQUALS( io::path::makeUnixPreferred( "C:\\a/b/c\\d" ), "C:/a/b/c/d", )
+    ASSERT_EQUALS( io::path::makeUnixPreferred( "a\\b/c\\d" ), "a/b/c/d", )
+    ASSERT_EQUALS( io::path::makeUnixPreferred( "" ), "", )
 }
 
 TEST_CASE( addSeparatorIfNeedTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "/a/b" ), "/a/b/", );
-    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "/a/b/" ), "/a/b/", );
-    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "/" ), "/", );
-    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "" ), "", );
+    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "/a/b" ), "/a/b/", )
+    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "/a/b/" ), "/a/b/", )
+    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "/" ), "/", )
+    ASSERT_EQUALS( io::path::addSeparatorToDirIfNeed( "" ), "", )
 }
 
 TEST_CASE( removeSeparatorIfNeedTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "/a/b" ), "/a/b", );
-    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "/a/b/" ), "/a/b", );
-    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "/" ), "/", );
-    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "" ), "", );
+    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "/a/b" ), "/a/b", )
+    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "/a/b/" ), "/a/b", )
+    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "/" ), "/", )
+    ASSERT_EQUALS( io::path::removeSeparatorFromDirIfNeed( "" ), "", )
 }
 
 TEST_CASE( parentPathTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::parentPath( "a/b/c" ), "a/b", );
-    ASSERT_EQUALS( io::path::parentPath( "a" ), "", );
-    ASSERT_EQUALS( io::path::parentPath( "/a" ), "/", );
-    ASSERT_EQUALS( io::path::parentPath( "/" ), "", );
+    ASSERT_EQUALS( io::path::parentPath( "a/b/c" ), "a/b", )
+    ASSERT_EQUALS( io::path::parentPath( "a" ), "", )
+    ASSERT_EQUALS( io::path::parentPath( "/a" ), "/", )
+    ASSERT_EQUALS( io::path::parentPath( "/" ), "", )
 }
 
 TEST_CASE( absolutePathTest, IOPathTests ) {
     string currpath = io::path::currentPath();
 
-    ASSERT_EQUALS( io::path::absolutePath( "C:/a/b/c" ), "C:/a/b/c", );
-    ASSERT_EQUALS( io::path::absolutePath( "/a/b/c" ), "/a/b/c", );
-    ASSERT_EQUALS( io::path::absolutePath( "a/b/c" ), currpath + "/a/b/c", );
+    ASSERT_EQUALS( io::path::absolutePath( "C:/a/b/c" ), "C:/a/b/c", )
+    ASSERT_EQUALS( io::path::absolutePath( "/a/b/c" ), "/a/b/c", )
+    ASSERT_EQUALS( io::path::absolutePath( "a/b/c" ), currpath + "/a/b/c", )
 
-    ASSERT_EQUALS( io::path::absolutePath( "" ), currpath, );
-    ASSERT_EQUALS( io::path::absolutePath( "." ), currpath, );
-    ASSERT_EQUALS( io::path::absolutePath( "./" ), currpath, );
+    ASSERT_EQUALS( io::path::absolutePath( "" ), currpath, )
+    ASSERT_EQUALS( io::path::absolutePath( "." ), currpath, )
+    ASSERT_EQUALS( io::path::absolutePath( "./" ), currpath, )
 }
 
 TEST_CASE( relativePathTest, IOPathTests ) {    
     string currpath = io::path::currentPath();
 
-    ASSERT_EQUALS( io::path::relativePath( io::path::absolutePath( "a/b/c" ) ), "a/b/c", );
-    ASSERT_EQUALS( io::path::relativePath( currpath + "/a/b/c" ), "a/b/c", );
-    ASSERT_EQUALS( io::path::relativePath( currpath ), ".", );
-    ASSERT_EQUALS( io::path::relativePath( currpath + "/." ), ".", );
-    ASSERT_EQUALS( io::path::relativePath( currpath + "/./" ), ".", );
+    ASSERT_EQUALS( io::path::relativePath( io::path::absolutePath( "a/b/c" ) ), "a/b/c", )
+    ASSERT_EQUALS( io::path::relativePath( currpath + "/a/b/c" ), "a/b/c", )
+    ASSERT_EQUALS( io::path::relativePath( currpath ), ".", )
+    ASSERT_EQUALS( io::path::relativePath( currpath + "/." ), ".", )
+    ASSERT_EQUALS( io::path::relativePath( currpath + "/./" ), ".", )
 }
 
 TEST_CASE( extensionTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::extension( "abc.txt" ), "txt", );
-    ASSERT_EQUALS( io::path::extension( "abc.*" ), "*", );
-    ASSERT_EQUALS( io::path::extension( "abc" ), "", );
-    ASSERT_EQUALS( io::path::extension( "abc." ), "", );
-    ASSERT_EQUALS( io::path::extension( "" ), "", );
+    ASSERT_EQUALS( io::path::extension( "abc.txt" ), "txt", )
+    ASSERT_EQUALS( io::path::extension( "abc.*" ), "*", )
+    ASSERT_EQUALS( io::path::extension( "abc" ), "", )
+    ASSERT_EQUALS( io::path::extension( "abc." ), "", )
+    ASSERT_EQUALS( io::path::extension( "" ), "", )
 }
 
 TEST_CASE( jokerJokerDirPathTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::jokerJokerDirPath( "a/**/b/c" ), "a/b", );
-    ASSERT_EQUALS( io::path::jokerJokerDirPath( "a/b/c/**/" ), "a/b", );
-    ASSERT_EQUALS( io::path::jokerJokerDirPath( "**/a/b/c" ), "a/b", );
-    ASSERT_EQUALS( io::path::jokerJokerDirPath( "" ), "", );
+    ASSERT_EQUALS( io::path::jokerJokerDirPath( "a/**/b/c" ), "a/b", )
+    ASSERT_EQUALS( io::path::jokerJokerDirPath( "a/b/c/**/" ), "a/b", )
+    ASSERT_EQUALS( io::path::jokerJokerDirPath( "**/a/b/c" ), "a/b", )
+    ASSERT_EQUALS( io::path::jokerJokerDirPath( "" ), "", )
 }
 
 TEST_CASE( jokerJokerBeforePathTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**/c/d" ), "a/b/", );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "**/c/d" ), "", );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**/" ), "a/b/", );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**" ), "a/b/", );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "**" ), "", );
-    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "" ), "", );
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**/c/d" ), "a/b/", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "**/c/d" ), "", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**/" ), "a/b/", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "a/b/**" ), "a/b/", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "**" ), "", )
+    ASSERT_EQUALS( io::path::jokerJokerBeforePath( "" ), "", )
 }
 
 TEST_CASE( jokerJokerAfterPathTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "a/b/**/c/d" ), "c/d", );
-    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "**/c/d" ), "c/d", );
-    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "a/b/**/" ), "", );
-    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "a/b/**" ), "", );
-    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "**" ), "", );
-    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "" ), "", );
+    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "a/b/**/c/d" ), "c/d", )
+    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "**/c/d" ), "c/d", )
+    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "a/b/**/" ), "", )
+    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "a/b/**" ), "", )
+    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "**" ), "", )
+    ASSERT_EQUALS( io::path::jokerJokerAfterPath( "" ), "", )
 }
 
 TEST_CASE( removeJokerJokerTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::removeJokerJoker( "a/b/**/c/d" ), "a/b/c/d", );
-    ASSERT_EQUALS( io::path::removeJokerJoker( "a/**/b" ), "a/b", );
-    ASSERT_EQUALS( io::path::removeJokerJoker( "**/b" ), "b", );
-    ASSERT_EQUALS( io::path::removeJokerJoker( "a/b/**" ), "a/b/", );
-    ASSERT_EQUALS( io::path::removeJokerJoker( "a/b/c" ), "a/b/c", );
+    ASSERT_EQUALS( io::path::removeJokerJoker( "a/b/**/c/d" ), "a/b/c/d", )
+    ASSERT_EQUALS( io::path::removeJokerJoker( "a/**/b" ), "a/b", )
+    ASSERT_EQUALS( io::path::removeJokerJoker( "**/b" ), "b", )
+    ASSERT_EQUALS( io::path::removeJokerJoker( "a/b/**" ), "a/b/", )
+    ASSERT_EQUALS( io::path::removeJokerJoker( "a/b/c" ), "a/b/c", )
 }
 
 TEST_CASE( isFileNameStartsWithJoker, IOPathTests ) {
-    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "a/b/*.txt" ), );
-    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "a/b/*" ), );
-    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "*.txt" ), );
-    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "*" ), );
-    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "." ), );
-    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "a" ), );
-    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "" ), );
-    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "a/b/**/a" ), );
-    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "a/b/**/" ), );
-    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "**/" ), );
+    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "a/b/*.txt" ), )
+    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "a/b/*" ), )
+    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "*.txt" ), )
+    ASSERT_TRUE( io::path::isFileNameStartsWithJoker( "*" ), )
+    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "." ), )
+    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "a" ), )
+    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "" ), )
+    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "a/b/**/a" ), )
+    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "a/b/**/" ), )
+    ASSERT_FALSE( io::path::isFileNameStartsWithJoker( "**/" ), )
 }
 
 TEST_CASE( removeInitTwoDotsAndSlashesTest, IOPathTests ) {
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../a" ), "a", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../a/b" ), "a/b", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../" ), "", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../../../a" ), "a", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../../a/b" ), "a/b", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( ".." ), "", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "" ), "", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "a/b/c" ), "a/b/c", );
-    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "/a/b/" ), "/a/b/", );
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../a" ), "a", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../a/b" ), "a/b", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../" ), "", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../../../a" ), "a", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "../../a/b" ), "a/b", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( ".." ), "", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "" ), "", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "a/b/c" ), "a/b/c", )
+    ASSERT_EQUALS( io::path::removeInitTwoDotsAndSlashes( "/a/b/" ), "/a/b/", )
 }
 
 TEST_CASE( absoluteResolvePathTest, IOPathTests ) {
     string currpath = io::path::currentPath();
     currpath = io::path::parentPath( currpath );
     currpath = io::path::parentPath( currpath );
-    ASSERT_EQUALS( io::path::absoluteResolvePath( "../../d" ), currpath + "/d", );
+    ASSERT_EQUALS( io::path::absoluteResolvePath( "../../d" ), currpath + "/d", )
 }
 
 TEST_CASE( resolvePathTest1, IOPathTests ) {
-    ASSERT_EQUALS( io::path::resolvePath( "/a/b/c/../../d" ), "/a/d", );
-    ASSERT_EQUALS( io::path::resolvePath( "/a/../../d" ), "d", );
+    ASSERT_EQUALS( io::path::resolvePath( "/a/b/c/../../d" ), "/a/d", )
+    ASSERT_EQUALS( io::path::resolvePath( "/a/../../d" ), "d", )
 }
 
 TEST_CASE( resolvePathTest2, IOPathTests ) {
-    ASSERT_EQUALS( io::path::resolvePath( "/a/b/c", "../../d" ), "/a/d", );
-    ASSERT_EQUALS( io::path::resolvePath( "/a/", "../../d" ), "d", );
+    ASSERT_EQUALS( io::path::resolvePath( "/a/b/c", "../../d" ), "/a/d", )
+    ASSERT_EQUALS( io::path::resolvePath( "/a/", "../../d" ), "d", )
 }
 
 #endif
--- test/strutiltests.h
+++ test/strutiltests.h
@@ -10,109 +10,109 @@
 using std::vector;
 
 TEST_CASE( startsWithTest, StrutilTests ) {
-    ASSERT_TRUE( strutil::startsWith( "abc", "a" ), );
-    ASSERT_TRUE( strutil::startsWith( "_abc", "_" ), );
-    ASSERT_TRUE( strutil::startsWith( "(", "(" ), );
-	ASSERT_FALSE( strutil::startsWith( "abc", "bc" ), );
-	ASSERT_FALSE( strutil::startsWith( "", "bc" ), );
-	ASSERT_FALSE( strutil::startsWith( "", "AB" ), );
-	ASSERT_TRUE( strutil::startsWith( "", "" ), );
+    ASSERT_TRUE( strutil::startsWith( "abc", "a" ), )
+    ASSERT_TRUE( strutil::startsWith( "_abc", "_" ), )
+    ASSERT_TRUE( strutil::startsWith( "(", "(" ), )
+	ASSERT_FALSE( strutil::startsWith( "abc", "bc" ), )
+	ASSERT_FALSE( strutil::startsWith( "", "bc" ), )
+	ASSERT_FALSE( strutil::startsWith( "", "AB" ), )
+	ASSERT_TRUE( strutil::startsWith( "", "" ), )
 }
 
 TEST_CASE( endsWithTest, StrutilTests ) {
-    ASSERT_TRUE( strutil::endsWith( "abc", "bc" ), );
-    ASSERT_TRUE( strutil::endsWith( "abc*.txt", ".txt" ), );
-    ASSERT_TRUE( strutil::endsWith( "a", "a" ), );
-    ASSERT_TRUE( strutil::endsWith( "", "" ), );
-    ASSERT_TRUE( strutil::endsWith( "aaa", "" ), );
+    ASSERT_TRUE( strutil::endsWith( "abc", "bc" ), )
+    ASSERT_TRUE( strutil::endsWith( "abc*.txt", ".txt" ), )
+    ASSERT_TRUE( strutil::endsWith( "a", "a" ), )
+    ASSERT_TRUE( strutil::endsWith( "", "" ), )
+    ASSERT_TRUE( strutil::endsWith( "aaa", "" ), )
 
-    ASSERT_FALSE( strutil::endsWith( "abc", "ac" ), );
-    ASSERT_FALSE( strutil::endsWith( "abc*.txt", "txx" ), );
-    ASSERT_FALSE( strutil::endsWith( "", "a" ), );
-    ASSERT_FALSE( strutil::endsWith( "a", "ab" ), );
+    ASSERT_FALSE( strutil::endsWith( "abc", "ac" ), )
+    ASSERT_FALSE( strutil::endsWith( "abc*.txt", "txx" ), )
+    ASSERT_FALSE( strutil::endsWith( "", "a" ), )
+    ASSERT_FALSE( strutil::endsWith( "a", "ab" ), )
 }
 
 TEST_CASE( endsWithSomeTest, StrutilTests ) {
-    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", ".cc .cpp .c" ), );
-    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", ".cpp .c .cc" ), );
-    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", ".cc .c .cpp" ), );
-    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", "  .cc    .cpp  .c   " ), );
+    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", ".cc .cpp .c" ), )
+    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", ".cpp .c .cc" ), )
+    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", ".cc .c .cpp" ), )
+    ASSERT_TRUE( strutil::endsWithSome( "abc.cpp", "  .cc    .cpp  .c   " ), )
 
-    ASSERT_FALSE( strutil::endsWithSome( "abc.cpp", "" ), );
-    ASSERT_FALSE( strutil::endsWithSome( "abc.cpp", ".cc .tpp .h .c" ), );
+    ASSERT_FALSE( strutil::endsWithSome( "abc.cpp", "" ), )
+    ASSERT_FALSE( strutil::endsWithSome( "abc.cpp", ".cc .tpp .h .c" ), )
 }
 
 TEST_CASE( isEqualToSomeTest, StrutilTests ) {
-    ASSERT_TRUE( strutil::isEqualToSome( "abc", "a ab abc cba" ), ); 
-    ASSERT_TRUE( strutil::isEqualToSome( "abc", " a   ab  abc  cba  " ), ); 
-    ASSERT_FALSE( strutil::isEqualToSome( "abc", "a ab cba" ), ); 
-    ASSERT_FALSE( strutil::isEqualToSome( "abc", "" ), ); 
+    ASSERT_TRUE( strutil::isEqualToSome( "abc", "a ab abc cba" ), ) 
+    ASSERT_TRUE( strutil::isEqualToSome( "abc", " a   ab  abc  cba  " ), ) 
+    ASSERT_FALSE( strutil::isEqualToSome( "abc", "a ab cba" ), ) 
+    ASSERT_FALSE( strutil::isEqualToSome( "abc", "" ), ) 
 }
 
 TEST_CASE( isNextSubstrTest, StrutilTests ) {
-    ASSERT_TRUE( strutil::isNextSubstr( "abc def", 4, "def" ), );
-    ASSERT_TRUE( strutil::isNextSubstr( "   abc   def  ", 9, "def" ), );
+    ASSERT_TRUE( strutil::isNextSubstr( "abc def", 4, "def" ), )
+    ASSERT_TRUE( strutil::isNextSubstr( "   abc   def  ", 9, "def" ), )
     
-    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 5, "def" ), );
-    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 4, "def" ), );
-    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 4, "deg" ), );
-    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", -1, "def" ), );
-    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 20, "def" ), );
+    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 5, "def" ), )
+    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 4, "def" ), )
+    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 4, "deg" ), )
+    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", -1, "def" ), )
+    ASSERT_FALSE( strutil::isNextSubstr( "abc    def ghi", 20, "def" ), )
 }
 
 TEST_CASE( trimTests, StrutilTests ) {
-    ASSERT_EQUALS( strutil::trim( "  abc  " ), "abc", );
-    ASSERT_EQUALS( strutil::trim( "abc" ), "abc", );
-    ASSERT_EQUALS( strutil::trim( "" ), "", );
+    ASSERT_EQUALS( strutil::trim( "  abc  " ), "abc", )
+    ASSERT_EQUALS( strutil::trim( "abc" ), "abc", )
+    ASSERT_EQUALS( strutil::trim( "" ), "", )
 
-    ASSERT_NOT_EQUALS( strutil::trim( "abc" ), "abd", );
-    ASSERT_NOT_EQUALS( strutil::trim( "  abc  " ), "abd", );
-    ASSERT_NOT_EQUALS( strutil::trim( "  abc  " ), "", );
-    ASSERT_NOT_EQUALS( strutil::trim( "" ), "a", );
-    ASSERT_NOT_EQUALS( strutil::trim( "a" ), "", );
+    ASSERT_NOT_EQUALS( strutil::trim( "abc" ), "abd", )
+    ASSERT_NOT_EQUALS( strutil::trim( "  abc  " ), "abd", )
+    ASSERT_NOT_EQUALS( strutil::trim( "  abc  " ), "", )
+    ASSERT_NOT_EQUALS( strutil::trim( "" ), "a", )
+    ASSERT_NOT_EQUALS( strutil::trim( "a" ), "", )
 }
 
 TEST_CASE( replaceTest, StrutilTests ) {
-    ASSERT_EQUALS( strutil::replace( "abcde", "bc", "" ), "ade", );
-    ASSERT_EQUALS( strutil::replace( "  abc de  ", " ", "X" ), "X abc de  ", );
-    ASSERT_EQUALS( strutil::replace( "abcde ", " ", "X" ), "abcdeX", );
-    ASSERT_EQUALS( strutil::replace( "abcde ", "bb", "" ), "abcde ", );
+    ASSERT_EQUALS( strutil::replace( "abcde", "bc", "" ), "ade", )
+    ASSERT_EQUALS( strutil::replace( "  abc de  ", " ", "X" ), "X abc de  ", )
+    ASSERT_EQUALS( strutil::replace( "abcde ", " ", "X" ), "abcdeX", )
+    ASSERT_EQUALS( strutil::replace( "abcde ", "bb", "" ), "abcde ", )
 
-    ASSERT_NOT_EQUALS( strutil::replace( "abcde ", " ", "X" ), "abcde ", );
-    ASSERT_NOT_EQUALS( strutil::replace( "abcde ", "bc", "" ), "abcde ", );
+    ASSERT_NOT_EQUALS( strutil::replace( "abcde ", " ", "X" ), "abcde ", )
+    ASSERT_NOT_EQUALS( strutil::replace( "abcde ", "bc", "" ), "abcde ", )
 }
 
 TEST_CASE( replaceAllTest, StrutilTests ) {
-    ASSERT_EQUALS( strutil::replaceAll( "abcde", "bc", "" ), "ade", );
-    ASSERT_EQUALS( strutil::replaceAll( "  abc de  ", " ", "X" ), "XXabcXdeXX", );
-    ASSERT_EQUALS( strutil::replaceAll( "abcde ", " ", "X" ), "abcdeX", );
-    ASSERT_EQUALS( strutil::replaceAll( "abcde ", "bb", "" ), "abcde ", );
+    ASSERT_EQUALS( strutil::replaceAll( "abcde", "bc", "" ), "ade", )
+    ASSERT_EQUALS( strutil::replaceAll( "  abc de  ", " ", "X" ), "XXabcXdeXX", )
+    ASSERT_EQUALS( strutil::replaceAll( "abcde ", " ", "X" ), "abcdeX", )
+    ASSERT_EQUALS( strutil::replaceAll( "abcde ", "bb", "" ), "abcde ", )
 
-    ASSERT_NOT_EQUALS( strutil::replaceAll( "abcde ", " ", "X" ), "abcde ", );
-    ASSERT_NOT_EQUALS( strutil::replaceAll( "abcde ", "bc", "" ), "abcde ", );
-    ASSERT_NOT_EQUALS( strutil::replaceAll( "  abc de  ", " ", "X" ), "XXabcXdeX ", );
+    ASSERT_NOT_EQUALS( strutil::replaceAll( "abcde ", " ", "X" ), "abcde ", )
+    ASSERT_NOT_EQUALS( strutil::replaceAll( "abcde ", "bc", "" ), "abcde ", )
+    ASSERT_NOT_EQUALS( strutil::replaceAll( "  abc de  ", " ", "X" ), "XXabcXdeX ", )
 }
 
 TEST_CASE( isWhiteSpaceTest, StrutilTests ) {
-    ASSERT_TRUE( strutil::isWhiteSpace( ' ' ), );
-    ASSERT_TRUE( strutil::isWhiteSpace( '\n' ), );
-    ASSERT_TRUE( strutil::isWhiteSpace( '\t' ), );
-    ASSERT_TRUE( strutil::isWhiteSpace( '\r' ), );
+    ASSERT_TRUE( strutil::isWhiteSpace( ' ' ), )
+    ASSERT_TRUE( strutil::isWhiteSpace( '\n' ), )
+    ASSERT_TRUE( strutil::isWhiteSpace( '\t' ), )
+    ASSERT_TRUE( strutil::isWhiteSpace( '\r' ), )
 
-    ASSERT_FALSE( strutil::isWhiteSpace( 'a' ), );
-    ASSERT_FALSE( strutil::isWhiteSpace( '\0' ), );
-    ASSERT_FALSE( strutil::isWhiteSpace( '\\' ), );
+    ASSERT_FALSE( strutil::isWhiteSpace( 'a' ), )
+    ASSERT_FALSE( strutil::isWhiteSpace( '\0' ), )
+    ASSERT_FALSE( strutil::isWhiteSpace( '\\' ), )
 }
 
 TEST_CASE( removeStartWithSpacesTest, StrutilTests ) {
-    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "  abc" ), "abc", );
-    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "abc" ), "abc", );
-    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "\n\t \t \rabc" ), "abc", );
-    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "\n\tX\t \rabc" ), "X\t \rabc", );
-    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "" ), "", );
+    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "  abc" ), "abc", )
+    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "abc" ), "abc", )
+    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "\n\t \t \rabc" ), "abc", )
+    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "\n\tX\t \rabc" ), "X\t \rabc", )
+    ASSERT_EQUALS( strutil::removeStartWhiteSpaces( "" ), "", )
 
-    ASSERT_NOT_EQUALS( strutil::removeStartWhiteSpaces( "\n\tX\t \rabc" ), "abc", );
-    ASSERT_NOT_EQUALS( strutil::removeStartWhiteSpaces( "\n\tX\t \rabc" ), "\n\tX\t \rabc", );
+    ASSERT_NOT_EQUALS( strutil::removeStartWhiteSpaces( "\n\tX\t \rabc" ), "abc", )
+    ASSERT_NOT_EQUALS( strutil::removeStartWhiteSpaces( "\n\tX\t \rabc" ), "\n\tX\t \rabc", )
 }
 
 TEST_CASE( splitWithDoubleQuotesTest, StrutilTests ) {
@@ -128,15 +128,15 @@ TEST_CASE( splitWithDoubleQuotesTest, StrutilTests ) {
     string str4 = "";
     vector<string> split4 = {};
 
-    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str1 ), split1, );
-    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str2 ), split2, );
-    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str3 ), split3, );
-    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str4 ), split4, );
+    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str1 ), split1, )
+    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str2 ), split2, )
+    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str3 ), split3, )
+    ASSERT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str4 ), split4, )
 
     string str5 = "a \"b c\" d";
     vector<string> split5 = { "a", "b", "c", "d" };
 
-    ASSERT_NOT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str5 ), split5, );
+    ASSERT_NOT_EQUALS_VECTORS( strutil::splitWithDoubleQuotes( str5 ), split5, )
 }
 
 #endif
