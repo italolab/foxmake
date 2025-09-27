224f8be33861e9a7ca4d0ffd088750e3fe8ca408
Ítalo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2025-09-27 05:58:03 -0300

    build: Criando o Build da Versão Para Windows no Linux

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 VERSION=1.2
-SO=linux
-ARCH=x64
 
 PROJ_NAME=foxmake
 
@@ -14,9 +12,11 @@ DEB_CONTROL_DESCRIPTION="Trata-se de um software automatizador de tarefas de bui
 APP_FILE_NAME=$PROJ_NAME
 
 BUILD_FOLDER=build
-APP_FOLDER=$PROJ_NAME-$VERSION-$SO-$ARCH
+APP_FOLDER=$PROJ_NAME-$VERSION-linux-x64
+WINDOWS_APP_FOLDER=$PROJ_NAME-$VERSION-windows-x64
 
 APP_DIR=$BUILD_FOLDER/$APP_FOLDER
+WINDOWS_APP_DIR=$BUILD_FOLDER/$WINDOWS_APP_FOLDER
 
 APP_FILE=$APP_DIR/$APP_FILE_NAME
 
@@ -41,25 +41,39 @@ if [ -f "$APP_DIR.tar.gz" ]; then
     echo "Removido: $APP_DIR.tar.gz"
 fi
 
+if [ -f "$WINDOWS_APP_DIR.zip" ]; then
+    rm "$WINDOWS_APP_DIR.zip"
+    echo "Removido: $WINDOWS_APP_DIR.zip"
+fi
+
 if [ -f "$APP_DIR.deb" ]; then
     rm "$APP_DIR.deb"
     echo "Removido: $APP_DIR.deb"
 fi
 
 mkdir -p $APP_DIR
+mkdir -p $WINDOWS_APP_DIR
 mkdir -p $DEB_APP_DIR
-
-echo
 
 # COMPILANDO...
 
-./foxmake buildall --script=FoxMakefile -var target=release
+echo
+echo Compilando...
+./foxmake $1 --script=FoxMakefile -var target=release
+
+echo
+echo Compilando para windows...
+./foxmake $1 --script=FoxMakefile-windows
 
 # COPIANDO LIB E INCLUDE PASTAS PARA A PASTA DO APLICATIVO EM BUILD
 
-cp -r lib include $APP_DIR
+cp -r lib/linux $APP_DIR/lib
+cp -r lib/windows $WINDOWS_APP_DIR/lib
 
-# EMPACOTANDO EM .TAR.GZ
+cp -r include $APP_DIR
+cp -r include $WINDOWS_APP_DIR
+
+# EMPACOTANDO VERSÃO x86_64 EM .TAR.GZ
 
 cd $BUILD_FOLDER
 
@@ -73,6 +87,22 @@ cd - &> /dev/null
 
 echo
 
+# EMPACOTANDO VERSÃO WINDOWS EM .ZIP
+
+cd $BUILD_FOLDER
+
+echo
+echo "Empacotando... $WINDOWS_APP_DIR.zip"
+
+zip -r -q "$WINDOWS_APP_FOLDER.zip" $WINDOWS_APP_FOLDER
+echo "Finalizado." 
+
+cd - &> /dev/null
+
+echo
+
+
+# GERANDO ARQUIVO .DEB
 
 # Copiando executável para pasta /usr/bin do .DEB
 
@@ -80,26 +110,34 @@ mkdir -p $DEB_APP_DIR/usr/bin
 
 cp $APP_FILE $DEB_APP_DIR/usr/bin/$APP_FILE_NAME
 
+# Copiando a static library do xutest para pasta /usr/lib do .DEB
 
-# GERANDO O ARQUIVO CONTROL DO .DEB
+mkdir -p $DEB_APP_DIR/usr/lib/x86_64-linux-gnu
+cp $APP_DIR/lib/libxutest.a $DEB_APP_DIR/usr/lib/x86_64-linux-gnu/libxutest.a
+
+# Copiando os headers para pasta /usr/include do .DEB
+
+mkdir -p $DEB_APP_DIR/usr/include
+cp -r $APP_DIR/include/* $DEB_APP_DIR/usr/include
+
+# Gerando arquivo control do .DEB
 
 mkdir -p $DEB_APP_DIR/DEBIAN
 
 echo "Package: $DEB_CONTROL_PACKAGE_NAME" >> $DEB_CONTROL_FILE
 echo "Version: $VERSION" >> $DEB_CONTROL_FILE
-echo "Section: games" >> $DEB_CONTROL_FILE
+echo "Section: contrib" >> $DEB_CONTROL_FILE
 echo "Architecture: amd64" >> $DEB_CONTROL_FILE
 echo "Installed-Size: $DEB_CONTROL_INSTALLED_SIZE" >> $DEB_CONTROL_FILE
 echo "Maintainer: $DEB_CONTROL_MAINTAINER" >> $DEB_CONTROL_FILE
 echo "Description: $DEB_CONTROL_DESCRIPTION" >> $DEB_CONTROL_FILE
 
-# EMPACOTANDO EM .DEB
+# Empacotando em .DEB
 
 echo
 echo "Empacotando... $APP_DIR.deb"
 dpkg-deb -b "$DEB_APP_DIR/" "$APP_DIR.deb"
 echo "Finalizado."
-
 
 # BUILD CONCLUIDO
 
